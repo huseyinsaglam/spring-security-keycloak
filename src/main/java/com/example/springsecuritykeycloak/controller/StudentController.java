27300af3a885ec6b832911c25d8a21f5e23a6010
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2023-07-30 01:21:45 +0300

    authication ve authorization

--- src/main/java/com/example/springsecuritykeycloak/controller/StudentController.java
+++ src/main/java/com/example/springsecuritykeycloak/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritykeycloak.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,5 +12,19 @@ public class StudentController {
 	public ModelAndView login() {
 		ModelAndView modelAndView = new ModelAndView("login");
 		return modelAndView; // /login ile gelindigi zaman login.html sayfasının gorunumu sunacaktır.
+	}
+
+	@GetMapping("/home")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("home");
+		return modelAndView;
+	}
+
+	@GetMapping("/manage-students")
+	@PreAuthorize("hasAuthority('PROFESSOR')")
+	//@PreAuthorize("hasAuthority('PROFESSOR') or hasAuthority('STUDENT')")
+	public ModelAndView manageStudents() {
+		ModelAndView modelAndView = new ModelAndView("manage-students");
+		return modelAndView;
 	}
 }
--- src/main/java/com/example/springsecuritykeycloak/security/SpringSecurityConfig.java
+++ src/main/java/com/example/springsecuritykeycloak/security/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,22 +12,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	UserDetailsServiceImpl userDetailsServiceImpl;
 
+	// HTTP isteklerine yönelik yetkilendirme kurallarını burada belirlenir.
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/login").permitAll()
+		http.authorizeRequests().antMatchers("/", "/login","/h2-console/**").permitAll()
 		.antMatchers("/home").authenticated()
 		.and().csrf().disable()
+				.headers().frameOptions().sameOrigin().and()
 		.formLogin().loginPage("/login")
-		.defaultSuccessUrl("/home")
-		.usernameParameter("username")
-		.passwordParameter("password");
+		.defaultSuccessUrl("/home");
+
 	}
 
+	// Kullanıcıları ve rolleri burada tanımlanır.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Kullanıcı detaylarını veritabanından alacak olan servisi belirtiyoruz
